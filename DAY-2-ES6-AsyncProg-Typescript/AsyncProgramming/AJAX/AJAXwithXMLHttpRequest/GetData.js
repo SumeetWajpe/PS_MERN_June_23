92471ff18848707da856c2d2fd38c6138b873b8f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-20 10:43:08 +0530

    ✨ Using Callback methods

--- DAY-2-ES6-AsyncProg-Typescript/AsyncProgramming/AJAX/AJAXwithXMLHttpRequest/GetData.js
+++ DAY-2-ES6-AsyncProg-Typescript/AsyncProgramming/AJAX/AJAXwithXMLHttpRequest/GetData.js
@@ -1,14 +1,14 @@
-function GetData() {
+function GetData(callback) {
   //1. create an XMLHttpRequest instance
   //2. open a connection
   //3. know if the request is completed
   //4. make the request
 
   var xmlHttpReq = new XMLHttpRequest();
-  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/postsss"); // establish a connection
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts"); // establish a connection
   xmlHttpReq.onreadystatechange = function () {
     if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
-      console.log(xmlHttpReq.responseText);
+      callback(xmlHttpReq.responseText);
     } else if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 404) {
       console.log("Error ! ", xmlHttpReq.status);
     }
