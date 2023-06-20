f330e6128a396de899e415f8873c6987b2daae9c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-20 15:16:42 +0530

    ✨ Functions - parameter types & return types

--- DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.js
+++ DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.js
@@ -3,3 +3,17 @@ console.log("Hello Typescript !");
 // x = "Hello";
 // console.log(typeof x);
 var n; // Type annotation
+var str;
+str = "Typescript !";
+var b;
+var s; // bad
+s = 10;
+s = "Hello";
+s = {};
+s = [];
+// Functions
+function Add(x, y) {
+    return x + y;
+}
+var result = Add(20, 30);
+console.log(result);
--- DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.ts
+++ DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.ts
@@ -4,3 +4,20 @@ console.log("Hello Typescript !");
 // console.log(typeof x);
 
 let n: number; // Type annotation
+let str: string;
+str = "Typescript !";
+let b: boolean;
+let s; // bad
+s = 10;
+s = "Hello";
+s = {};
+s = [];
+
+// Functions
+
+function Add(x: number, y: number): number {
+  return x + y;
+}
+
+let result: number = Add(20, 30);
+console.log(result);
