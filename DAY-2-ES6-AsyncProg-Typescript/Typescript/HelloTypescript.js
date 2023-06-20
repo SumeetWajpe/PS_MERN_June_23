2367be162677d068c0d63000c523c6443b26fa66
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-20 16:58:51 +0530

    ✨ Using tsconfig.json

--- DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.js
+++ DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.js
@@ -2,11 +2,11 @@ console.log("Hello Typescript !");
 //let x = 100; // Type inference
 // x = "Hello";
 // console.log(typeof x);
-var n; // Type annotation
-var str;
+let n; // Type annotation
+let str;
 str = "Typescript !";
-var b;
-var s; // bad
+let b;
+let s; // bad
 s = 10;
 s = "Hello";
 s = {};
@@ -17,55 +17,43 @@ s = [];
 // }
 // let result: number = Add(20, 30);
 // console.log(result);
-var Add = function (x, y) { return x + y; };
-var result = Add(50, 30);
+var Add = (x, y) => x + y;
+let result = Add(50, 30);
 console.log(result);
 // Optional arguments
 // function PrintBook(author?: string, title?: string, publication?: string) {
 //   console.log(author, title, publication);
 // }
 // Default Arguments
-function PrintBook(author, title, publication) {
-    if (author === void 0) { author = "Unknown"; }
-    if (title === void 0) { title = "Unknown"; }
-    if (publication === void 0) { publication = "Unknown"; }
+function PrintBook(author = "Unknown", title = "Unknown", publication = "Unknown") {
     console.log(author, title, publication);
 }
 PrintBook();
 //var person: IPerson = { name: "Virat", sport: "Cricket", xyz: 123 };
-var CPerson = /** @class */ (function () {
-    function CPerson() {
-    }
-    CPerson.prototype.getDetails = function () {
+class CPerson {
+    getDetails() {
         return "Hello";
-    };
-    return CPerson;
-}());
+    }
+}
 // var person: Person = { name: "Virat", sport: "Cricket", xyz: 123 };
-var Car = /** @class */ (function () {
-    function Car(name, speed) {
-        if (name === void 0) { name = "BMW"; }
-        if (speed === void 0) { speed = 200; }
+class Car {
+    constructor(name = "BMW", speed = 200) {
         this.name = name;
         this.speed = speed;
     }
-    return Car;
-}());
+}
 var carObj = new Car();
 console.log(carObj.speed);
 // let carsColl: Car[] = [new Car()];
-var carsColl = new Array(new Car(), new Car());
+let carsColl = new Array(new Car(), new Car());
 // OR
 // Enhanced class syntax
-var EnhancedCar = /** @class */ (function () {
-    function EnhancedCar(name, speed) {
-        if (name === void 0) { name = "Audi"; }
-        if (speed === void 0) { speed = 300; }
+class EnhancedCar {
+    constructor(name = "Audi", speed = 300) {
         this.name = name;
         this.speed = speed;
     }
-    return EnhancedCar;
-}());
+}
 var enhancedCarObj = new EnhancedCar();
 // Generics
 // var cars: string[] = ["BMW", "AUDI", "MERC", 10]; // Error
@@ -73,18 +61,15 @@ var enhancedCarObj = new EnhancedCar();
 var carsObj = new Array("TATA", "MAHINDRA");
 // Generic Function
 function Swap(x, y) {
-    var t;
+    let t;
     t = x;
     x = y;
     y = t;
 }
 Swap(20, 30);
 Swap("Hello", "World");
-var Point = /** @class */ (function () {
-    function Point() {
-    }
-    return Point;
-}());
+class Point {
+}
 var point = new Point();
 // class Book -> id, title, author
 // coll of 5 books (array)
--- DAY-2-ES6-AsyncProg-Typescript/Typescript/Modules/Calc.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Math_js_1 = require("./Math.js");
-console.log("The addition is :  ".concat((0, Math_js_1.Add)(20, 50)));
