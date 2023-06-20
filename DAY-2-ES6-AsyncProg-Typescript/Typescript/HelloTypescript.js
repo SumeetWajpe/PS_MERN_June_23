33993cfb645fd6fd3c6fed7180147221e8ee2d9a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-20 15:07:42 +0530

    ✨ Using Type annotations

---
+++ DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.js
@@ -0,0 +1,5 @@
+console.log("Hello Typescript !");
+//let x = 100; // Type inference
+// x = "Hello";
+// console.log(typeof x);
+var n; // Type annotation
---
+++ DAY-2-ES6-AsyncProg-Typescript/Typescript/HelloTypescript.ts
@@ -0,0 +1,6 @@
+console.log("Hello Typescript !");
+//let x = 100; // Type inference
+// x = "Hello";
+// console.log(typeof x);
+
+let n: number; // Type annotation
