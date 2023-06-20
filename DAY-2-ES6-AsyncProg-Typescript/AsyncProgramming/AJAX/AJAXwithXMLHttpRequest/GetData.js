89ac31fa70b54a2ee6d780192b2efba781c93d10
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-20 10:30:10 +0530

    ✨ Handling error

--- DAY-2-ES6-AsyncProg-Typescript/AsyncProgramming/AJAX/AJAXwithXMLHttpRequest/GetData.js
+++ DAY-2-ES6-AsyncProg-Typescript/AsyncProgramming/AJAX/AJAXwithXMLHttpRequest/GetData.js
@@ -5,10 +5,12 @@ function GetData() {
   //4. make the request
 
   var xmlHttpReq = new XMLHttpRequest();
-  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts"); // establish a connection
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/postsss"); // establish a connection
   xmlHttpReq.onreadystatechange = function () {
     if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
       console.log(xmlHttpReq.responseText);
+    } else if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 404) {
+      console.log("Error ! ", xmlHttpReq.status);
     }
   };
   xmlHttpReq.send(); // places the async call
