6be432eab7c2f8e49d5b0e3958021aba9c2fce8e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-20 10:17:20 +0530

    ✨ AJAX basics

--- DAY-2-ES6-AsyncProg-Typescript/AsyncProgramming/AJAX/AJAXwithXMLHttpRequest/GetData.js
+++ DAY-2-ES6-AsyncProg-Typescript/AsyncProgramming/AJAX/AJAXwithXMLHttpRequest/GetData.js
@@ -1,3 +1,15 @@
 function GetData() {
-  console.log("Logic for getting data async");
+  //1. create an XMLHttpRequest instance
+  //2. open a connection
+  //3. know if the request is completed
+  //4. make the request
+
+  var xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts"); // establish a connection
+  xmlHttpReq.onreadystatechange = function () {
+    if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
+      console.log(xmlHttpReq.responseText);
+    }
+  };
+  xmlHttpReq.send(); // places the async call
 }
