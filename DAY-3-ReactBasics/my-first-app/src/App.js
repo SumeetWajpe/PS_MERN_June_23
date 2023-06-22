f66f6180ec41af90d18c2cc9f6d72a0d3a0d8e43
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-22 14:12:25 +0530

    ✨ Iterating on an array - map

--- DAY-3-ReactBasics/my-first-app/src/App.js
+++ DAY-3-ReactBasics/my-first-app/src/App.js
@@ -2,17 +2,34 @@ import React from "react";
 import { Message } from "./components/message.component";
 
 class App extends React.Component {
-  messageOne = {
-    msg: "Hello",
-    from: "Joe",
-    to: "Modi",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Hello_Web_Series_%28Wordmark%29_Logo.png/1200px-Hello_Web_Series_%28Wordmark%29_Logo.png",
-  };
+  messages = [
+    {
+      msg: "Hello",
+      from: "Joe",
+      to: "Modi",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Hello_Web_Series_%28Wordmark%29_Logo.png/1200px-Hello_Web_Series_%28Wordmark%29_Logo.png",
+    },
+    {
+      msg: "Hey",
+      from: "Jim",
+      to: "Kim",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Hey_2018_logo.svg/1200px-Hey_2018_logo.svg.png",
+    },
+    {
+      msg: "Bye",
+      from: "John",
+      to: "Jim",
+      imageUrl: "https://chd.in/wp-content/uploads/2018/05/bye.png",
+    },
+  ];
   render() {
     return (
       <div>
-        <Message details={this.messageOne} />
+        {this.messages.map(message => (
+          <Message details={message} />
+        ))}
         {/* <Message
           msg="Hey"
           from="Jim"
