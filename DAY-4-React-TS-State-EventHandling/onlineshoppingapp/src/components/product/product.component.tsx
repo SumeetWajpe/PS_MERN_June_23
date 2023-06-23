b085fd7907e92b9ea8130b610c2192493d236126
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-23 11:31:02 +0530

    ✨ Adding rating with fontawesome

--- DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/product/product.component.tsx
+++ DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/product/product.component.tsx
@@ -7,6 +7,16 @@ type ProductProps = {
 
 export class ProductComponent extends React.Component<ProductProps> {
   render(): React.ReactNode {
+    let ratings: React.ReactNode[] = [];
+    for (let i = 0; i < this.props.productdetails.rating; i++) {
+      ratings.push(
+        <i
+          className="fa fa-star"
+          style={{ color: "orange" }}
+          aria-hidden="true"
+        ></i>,
+      );
+    }
     return (
       <div className="col-md-3 my-1">
         <div className="card">
@@ -18,8 +28,9 @@ export class ProductComponent extends React.Component<ProductProps> {
           />
           <div className="card-body">
             <h5 className="card-title">{this.props.productdetails.title}</h5>
+            <p className="card-text m-0">{ratings}</p>
+
             <p className="card-text m-0">{this.props.productdetails.price}</p>
-            <p className="card-text m-0">{this.props.productdetails.rating}</p>
             <button className="btn btn-outline-primary">
               {this.props.productdetails.likes}{" "}
               <i className="fa fa-thumbs-up" aria-hidden="true"></i>
