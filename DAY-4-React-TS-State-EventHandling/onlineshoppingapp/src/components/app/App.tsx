f1960c16baa9d064c12b52304d36eb4deaba36df
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-23 09:45:38 +0530

    ✨ onlineshoppingapp-ts

---
+++ DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/app/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class App extends React.Component {
+  render(): React.ReactNode {
+    return <h1>List of Products</h1>; // List of Products
+  }
+}
+export default App;
