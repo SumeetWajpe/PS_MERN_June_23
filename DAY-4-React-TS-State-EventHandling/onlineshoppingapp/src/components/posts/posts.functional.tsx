6b5f894d0c4b4eead2add8cca013b0e167833f3f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-26 16:05:57 +0530

    ✨ Using strong typing with Posts Functional Component (useState)

--- DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/posts/posts.functional.tsx
+++ DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/posts/posts.functional.tsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { PostModel } from "../../models/post.model";
+
+type PostsState = {
+  posts: PostModel[];
+};
 
 export function Posts() {
+  let [postsData, setPostsData] = useState<PostsState>({ posts: [] });
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(res => res.json())
-      .then(posts => console.log(posts));
+      .then(posts => setPostsData({ posts }));
   });
   return (
     <>
       <header>
         <h1>Posts</h1>
+        <ul>
+          {postsData.posts.map(post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
       </header>
     </>
   );
