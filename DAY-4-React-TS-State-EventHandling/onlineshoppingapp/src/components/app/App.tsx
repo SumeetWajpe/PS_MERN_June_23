2b7190bcbc2d5fa0126be6fd87afc40cdf0b9ada
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-27 10:37:06 +0530

    ✨ using Links

--- DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/app/App.tsx
+++ DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/app/App.tsx
@@ -3,7 +3,7 @@ import { ListOfProducts } from "../listofproducts/listofproducts.component";
 import { Message } from "../functional/message.component";
 import { Counter } from "../functional/counter.component";
 import { Posts } from "../posts/posts.functional";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { NewProduct } from "../newproduct/newproduct.component";
 
 // export default function App() {
@@ -13,6 +13,8 @@ import { NewProduct } from "../newproduct/newproduct.component";
 const App: React.FC = function () {
   return (
     <BrowserRouter>
+      <Link to="/">Products</Link> | <Link to="/posts">Posts</Link> |{" "}
+      <Link to="/newproduct">New Product</Link>
       <Routes>
         <Route path="/" element={<ListOfProducts />} />
         <Route path="/posts" element={<Posts />} />
