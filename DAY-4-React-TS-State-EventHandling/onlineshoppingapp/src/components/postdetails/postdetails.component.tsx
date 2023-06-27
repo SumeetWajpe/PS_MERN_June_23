bbf61c608f3e7b231d4948c4a77c2762a70bb64d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-27 12:51:15 +0530

    ✨ Post Details - Conditional Rendering

--- DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/postdetails/postdetails.component.tsx
+++ DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/postdetails/postdetails.component.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { PostModel } from "../../models/post.model";
 
 export default function PostDetails() {
   const { id } = useParams();
+  const [thePost, setThePost] = useState<PostModel>(new PostModel());
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((post: PostModel) => setThePost(post));
+  }, []);
   return (
-    <div>
+    <div className="alert alert-secondary">
       <header>
         <h1>Post Details for {id}</h1>
       </header>
+      {thePost.title != "" ? (
+        <div>
+          <h2>Title : </h2> {thePost.title}
+          <h3>Body : </h3>
+          {thePost.body}
+        </div>
+      ) : (
+        "Loading.."
+      )}
     </div>
   );
 }
