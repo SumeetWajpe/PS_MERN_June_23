ee5197db2c8a8c6c9ebe17584e4d6b3672aed3ff
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-27 10:20:54 +0530

    ✨ CurrLikes useState implementation

--- DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/product/product.component.tsx
+++ DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/product/product.component.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { ProductModel } from "../../models/product.model";
 import { Rating } from "../rating/rating.component";
 
@@ -7,6 +7,7 @@ type ProductProps = {
 };
 
 export const ProductComponent: FC<ProductProps> = (props: ProductProps) => {
+  const [currLikes, setCurrLikes] = useState(props.productdetails.likes);
   return (
     <>
       <div className="col-md-3 my-1">
@@ -28,8 +29,12 @@ export const ProductComponent: FC<ProductProps> = (props: ProductProps) => {
             </p>
 
             <p className="card-text m-0">₹.{props.productdetails.price}</p>
-            <button className="btn btn-outline-primary">
-              {props.productdetails.likes} {/* {this.state.currLikes} */}
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => setCurrLikes(currLikes + 1)}
+            >
+              {/* {props.productdetails.likes} {this.state.currLikes} */}
+              {currLikes}
               <i className="fa fa-thumbs-up" aria-hidden="true"></i>
             </button>
             <button className="btn btn-outline-danger mx-1">
