817f4b8b736fe2baade35d66a6e142756013532a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-28 14:58:06 +0530

    ✨ react-redux | used provider | useSelector hook

--- DAY-7/Redux/onlineshoppingapp-redux/src/components/app/App.tsx
+++ DAY-7/Redux/onlineshoppingapp-redux/src/components/app/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const products = useSelector((store: any) => store.products);
   return (
     <div>
-      <h1>Using Redux</h1>
+      <h1>Using Redux - {products.length}</h1>
     </div>
   );
 }
