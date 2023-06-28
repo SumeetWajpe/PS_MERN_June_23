a9e9f876d8c98288e6c2cffea276c7cdc74159d6
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-28 15:47:15 +0530

    ✨ Strong Typing in useSelector

--- DAY-7/Redux/onlineshoppingapp-redux/src/components/app/App.tsx
+++ DAY-7/Redux/onlineshoppingapp-redux/src/components/app/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { AppState } from "../../redux/store/store";
 
 function App() {
-  const products = useSelector((store: any) => store.products);
+  const products = useSelector((store: AppState) => store.products);
   return (
     <div>
       <h1>Using Redux - {products.length}</h1>
