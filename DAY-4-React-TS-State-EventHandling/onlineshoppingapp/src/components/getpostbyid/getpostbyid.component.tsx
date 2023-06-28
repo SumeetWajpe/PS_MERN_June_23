bbe1fcbba55ba65ecdd57b5920b74e7acff0e4b5
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-28 09:50:21 +0530

    ✨ NewProductWithReactHookForm basic

--- DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/getpostbyid/getpostbyid.component.tsx
+++ DAY-4-React-TS-State-EventHandling/onlineshoppingapp/src/components/getpostbyid/getpostbyid.component.tsx
@@ -4,12 +4,26 @@ import { PostModel } from "../../models/post.model";
 export default function GetPostById() {
   const [postId, setPostId] = useState<number>(1);
   const [post, setPost] = useState<PostModel>(new PostModel());
+  //   useEffect(() => {
+  //     if (postId) {
+  //       fetch("https://jsonplaceholder.typicode.com/posts/" + postId)
+  //         .then(res => res.json())
+  //         .then((post: PostModel) => setPost(post));
+  //     }
+  //   }, [postId]);
+  // useEffect callback function cannot be async (can't return a promise)
   useEffect(() => {
-    if (postId) {
-      fetch("https://jsonplaceholder.typicode.com/posts/" + postId)
-        .then(res => res.json())
-        .then((post: PostModel) => setPost(post));
-    }
+    (async () => {
+      if (postId) {
+        let res = await fetch(
+          "https://jsonplaceholder.typicode.com/posts/" + postId,
+        );
+        if (res.ok) {
+          let post: PostModel = await res.json();
+          setPost(post);
+        }
+      }
+    })();
   }, [postId]);
   return (
     <div>
