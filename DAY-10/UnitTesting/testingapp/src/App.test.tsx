347fe448c0f1901a62d65cb09c8f633e51d7d6b3
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-04 16:52:10 +0530

    ✨ Unit Testing with Enzyme

--- DAY-10/UnitTesting/testingapp/src/App.test.tsx
+++ DAY-10/UnitTesting/testingapp/src/App.test.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
 import App from "./App";
 
 describe("suites for App component", () => {
   it("checks for App instance to be truthy", () => {
-    let appInstance = render(<App />);
+    let appInstance = shallow(<App />);
     expect(appInstance).toBeDefined();
   });
 });
