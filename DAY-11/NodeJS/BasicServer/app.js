34e3c7ffdfe0f7459e0384574a1674722b4d6ab8
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-05 15:57:22 +0530

    ✨ NodeJS serving .css files (req.url)

--- DAY-11/NodeJS/BasicServer/app.js
+++ DAY-11/NodeJS/BasicServer/app.js
@@ -5,7 +5,29 @@ const hostname = "127.0.0.1";
 const port = 4000;
 
 const server = http.createServer((req, res) => {
-  
+  // fetch data from Index.html
+  console.log(req.url);
+  if (req.url == "/") {
+    fs.readFile("Index.html", (err, data) => {
+      if (err) {
+        console.log("Error !", err);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data.toString());
+      }
+    });
+  } else if (req.url == "/styles.css") {
+    fs.readFile("styles.css", (err, data) => {
+      if (err) {
+        console.log("Error !", err);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/css");
+        res.end(data.toString());
+      }
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
