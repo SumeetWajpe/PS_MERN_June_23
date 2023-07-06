dc1fb5085d81e137abceb89993cd39f24a83c3ab
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-06 17:07:15 +0530

    ✨ Using Pipe

--- DAY-12/Streams/EventEmitter/fsaseventemitter.js
+++ DAY-12/Streams/EventEmitter/fsaseventemitter.js
@@ -7,15 +7,18 @@ var fs = require("fs");
 let readableStream = fs.createReadStream("Input.txt", "utf-8");
 let writableStream = fs.createWriteStream("Output.txt", "utf-8");
 
-let dataToBeWritten = "";
-readableStream.on("data", chunk => {
-  console.log("\n>>>>>>>>>>>>>>>>>>> CHUNK >>>>>>>>>>>>>\n");
-  dataToBeWritten += chunk;
-});
+// let dataToBeWritten = "";
+// readableStream.on("data", chunk => {
+//   console.log("\n>>>>>>>>>>>>>>>>>>> CHUNK >>>>>>>>>>>>>\n");
+//   dataToBeWritten += chunk;
+// });
 
-readableStream.on("end", () => {
-  writableStream.write(dataToBeWritten);
-  writableStream.end();
-});
+// readableStream.on("end", () => {
+//   // when the input.txt is read completely
+//   writableStream.write(dataToBeWritten);
+//   writableStream.end(); // closing the writable stream
+// });
+
+readableStream.pipe(writableStream);
 
 console.log("Ended !");
