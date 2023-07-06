c0b77b02ce3a8f3d130f069c1ef4a36ba99f008e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-06 10:04:07 +0530

    ✨ Express basics

---
+++ DAY-12/Express/ExpressBasics/src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 5555;
+
+app.get("/", (req, res) => {
+  //   res.send("<h1>Hello World!</h1>");
+  //   res.json([
+  //     { id: 1, title: "Laptop" },
+  //     { id: 2, title: "Shoes" },
+  //   ]);
+
+  res.sendFile("Index.html", { root: __dirname });
+});
+
+app.get("/styles.css", (req, res) => {
+  res.sendFile("styles.css", { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
