5d766b2965110b760bf0b99a29c45708114b95ed
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-10 16:12:04 +0530

    ✨ Inserting new product

--- DAY-13/onlineshopping-server-mongodb/dist/routes/products.route.js
+++ DAY-13/onlineshopping-server-mongodb/dist/routes/products.route.js
@@ -13,6 +13,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const products_model_1 = __importDefault(require("../models/products.model"));
 var router = express_1.default.Router();
 /* GET home page. */
@@ -29,16 +31,18 @@ router.get("/", function (req, res) {
         }
     });
 });
-router.post("/newproduct", (req, res) => {
+router.post("/newproduct", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        let newProduct = req.body; // populated by express.json() middleware
-        res.status(201).json(newProduct);
+        let newProductFromRequest = req.body; // populated by express.json() middleware
+        let newProductToBeInserted = new products_model_1.default(Object.assign({}, newProductFromRequest));
+        yield newProductToBeInserted.save();
+        res.status(201).json(newProductFromRequest);
     }
     catch (error) {
         console.log(error);
         res.status(500).json({ msg: "Something went wrong !" });
     }
-});
+}));
 router.delete("/product/:id", (req, res) => {
     try {
         let productId = parseInt(req.params.id);
@@ -48,42 +52,31 @@ router.delete("/product/:id", (req, res) => {
         console.log(error);
     }
 });
-router.get("/videos/:id", (req, res) => {
-    // send chunk of the video
-    // path to the video
-    // status code - 200 success |  201 success (created) | 206 success (partial content)
-    // range header
-    // fs -> read the video
-    // pipe (send the response to client)
-    // chunk size / range size -> 1 MB
-    // video -> src
-    // 1st request (range bytes=0-) -> response (1MB)
-    // 2nd request (range bytes=100001-200001)
-    // 3rd request (range bytes=200001-300000)
+router.get("/videos/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        // let productId: number = parseInt(req.params.id);
-        // let theProduct = data.products.find(p => p.id === productId);
-        // let videoPath = theProduct?.videoUrl || "";
-        // // let videoPath = "./videos/shoes.mp4";
-        // let vPath = path.resolve(videoPath);
-        // const videoSize = fs.statSync(vPath).size;
-        // const range = req.headers.range;
-        // const chunk_size = 10 ** 6; // 1MB
-        // const start = Number(range?.replace(/\D/g, ""));
-        // const end = Math.min(start + chunk_size, videoSize - 1);
-        // const contentLength = end - start + 1;
-        // const headers = {
-        //   "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        //   "Accept-Ranges": "bytes",
-        //   "Content-Type": "video/mp4",
-        //   "Content-Length": contentLength,
-        // };
-        // const videoStream = fs.createReadStream(videoPath, { start, end });
-        // res.writeHead(206, headers);
-        // videoStream.pipe(res);
+        let productId = parseInt(req.params.id);
+        let theProduct = yield products_model_1.default.findOne({ id: productId });
+        let videoPath = (theProduct === null || theProduct === void 0 ? void 0 : theProduct.videoUrl) || "";
+        // let videoPath = "./videos/shoes.mp4";
+        let vPath = path_1.default.resolve(videoPath);
+        const videoSize = fs_1.default.statSync(vPath).size;
+        const range = req.headers.range;
+        const chunk_size = Math.pow(10, 6); // 1MB
+        const start = Number(range === null || range === void 0 ? void 0 : range.replace(/\D/g, ""));
+        const end = Math.min(start + chunk_size, videoSize - 1);
+        const contentLength = end - start + 1;
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Type": "video/mp4",
+            "Content-Length": contentLength,
+        };
+        const videoStream = fs_1.default.createReadStream(videoPath, { start, end });
+        res.writeHead(206, headers);
+        videoStream.pipe(res);
     }
     catch (error) {
         console.log(error);
     }
-});
+}));
 exports.default = router;
--- DAY-13/onlineshopping-server-mongodb/routes/products.route.ts
+++ DAY-13/onlineshopping-server-mongodb/routes/products.route.ts
@@ -17,11 +17,15 @@ router.get("/", async function (req: Request, res: Response) {
   }
 });
 
-router.post("/newproduct", (req: Request, res: Response) => {
+router.post("/newproduct", async (req: Request, res: Response) => {
   try {
-    let newProduct = req.body; // populated by express.json() middleware
+    let newProductFromRequest = req.body; // populated by express.json() middleware
 
-    res.status(201).json(newProduct);
+    let newProductToBeInserted = new products({ ...newProductFromRequest });
+
+    await newProductToBeInserted.save();
+
+    res.status(201).json(newProductFromRequest);
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "Something went wrong !" });
@@ -38,42 +42,28 @@ router.delete("/product/:id", (req: Request, res: Response) => {
   }
 });
 
-router.get("/videos/:id", (req: Request, res: Response) => {
-  // send chunk of the video
-  // path to the video
-  // status code - 200 success |  201 success (created) | 206 success (partial content)
-  // range header
-  // fs -> read the video
-  // pipe (send the response to client)
-
-  // chunk size / range size -> 1 MB
-
-  // video -> src
-  // 1st request (range bytes=0-) -> response (1MB)
-  // 2nd request (range bytes=100001-200001)
-  // 3rd request (range bytes=200001-300000)
-
+router.get("/videos/:id", async (req: Request, res: Response) => {
   try {
-    // let productId: number = parseInt(req.params.id);
-    // let theProduct = data.products.find(p => p.id === productId);
-    // let videoPath = theProduct?.videoUrl || "";
-    // // let videoPath = "./videos/shoes.mp4";
-    // let vPath = path.resolve(videoPath);
-    // const videoSize = fs.statSync(vPath).size;
-    // const range = req.headers.range;
-    // const chunk_size = 10 ** 6; // 1MB
-    // const start = Number(range?.replace(/\D/g, ""));
-    // const end = Math.min(start + chunk_size, videoSize - 1);
-    // const contentLength = end - start + 1;
-    // const headers = {
-    //   "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    //   "Accept-Ranges": "bytes",
-    //   "Content-Type": "video/mp4",
-    //   "Content-Length": contentLength,
-    // };
-    // const videoStream = fs.createReadStream(videoPath, { start, end });
-    // res.writeHead(206, headers);
-    // videoStream.pipe(res);
+    let productId: number = parseInt(req.params.id);
+    let theProduct = await products.findOne({ id: productId });
+    let videoPath = theProduct?.videoUrl || "";
+    // let videoPath = "./videos/shoes.mp4";
+    let vPath = path.resolve(videoPath);
+    const videoSize = fs.statSync(vPath).size;
+    const range = req.headers.range;
+    const chunk_size = 10 ** 6; // 1MB
+    const start = Number(range?.replace(/\D/g, ""));
+    const end = Math.min(start + chunk_size, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Type": "video/mp4",
+      "Content-Length": contentLength,
+    };
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    res.writeHead(206, headers);
+    videoStream.pipe(res);
   } catch (error) {
     console.log(error);
   }
