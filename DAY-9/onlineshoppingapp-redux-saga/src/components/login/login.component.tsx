b40d5a7f0c3355cb4a3d1a9f83e401e3a2410eda
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-11 14:33:18 +0530

    ✨ Adding Login Component

--- DAY-9/onlineshoppingapp-redux-saga/src/components/login/login.component.tsx
+++ DAY-9/onlineshoppingapp-redux-saga/src/components/login/login.component.tsx
@@ -1,7 +1,43 @@
 import React from "react";
-
+import "./login.css";
 const Login: React.FC = () => {
-  return <div>Login Component</div>;
+  return (
+    <div className="wrapper fadeInDown">
+      <div id="formContent">
+        <div className="fadeIn first">
+          <img
+            src="https://www.iconpacks.net/icons/1/free-user-group-icon-296-thumb.png"
+            id="icon"
+            alt="User Icon"
+          />
+        </div>
+
+        <form>
+          <input
+            type="text"
+            id="login"
+            className="fadeIn second"
+            name="login"
+            placeholder="login"
+          />
+          <input
+            type="text"
+            id="password"
+            className="fadeIn third"
+            name="login"
+            placeholder="password"
+          />
+          <input type="submit" className="fadeIn fourth" value="Log In" />
+        </form>
+
+        <div id="formFooter">
+          <a className="underlineHover" href="#">
+            Forgot Password?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
