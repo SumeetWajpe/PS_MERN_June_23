bf14230a235e17a9f2814384b8b9a2d6d03503b3
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-11 09:40:26 +0530

    ✨ Emitting custom events

--- DAY-14/CustomEventEmitter/app.js
+++ DAY-14/CustomEventEmitter/app.js
@@ -2,14 +2,38 @@
 let EventEmitter = require("events").EventEmitter;
 function GetCount(maxCount) {
   let e = new EventEmitter();
+  let count = 0;
   process.nextTick(() => {
-    e.emit("count", 1);
+    e.emit("start");
+    let t = setInterval(() => {
+      if (count < maxCount) {
+        if (count == 8) {
+          e.emit("error", count);
+          clearInterval(t);
+        } else {
+          e.emit("count", ++count);
+        }
+      } else if (count == maxCount) {
+        e.emit("end", count);
+        clearInterval(t);
+      }
+    }, 500);
   });
   return e;
 }
 
 // subscriber
 let evt = GetCount(10);
+evt.on("start", () => {
+  console.log("Counting started..");
+});
 evt.on("count", currCount => {
   console.log("Current count : " + currCount);
 });
+evt.on("error", currCount => {
+  console.log("Ended with err : " + currCount);
+});
+evt.on("end", currCount => {
+  console.log("Ended with Current count : " + currCount);
+});
+console.log("Program Ended !");
