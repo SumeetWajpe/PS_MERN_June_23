df1e0d6fbd41b6c338c21d9d1f8140b57d31f0f5
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-12 12:46:37 +0530

    ✨ Adding Custom Types | Collection | NotNull constraint

--- DAY-15/graphqlBasics/onlineshopping-app-graphql/src/index.ts
+++ DAY-15/graphqlBasics/onlineshopping-app-graphql/src/index.ts
@@ -5,16 +5,25 @@ const typeDefs = `#graphql
 
     #rootQuery
     type Query{
-        hello:String,
-        bye:String
+        products:[Product] # [] -> collection | array
+    }
+ # ! -> not null constraint
+    type Product{
+        id:ID!,
+        title:String
+        price:Int
     }
 
 `;
 
+let productsData = [
+  { id: 1, title: "Laptop", price: 25000 },
+  { id: 2, title: "Watch", price: 2500 },
+];
+
 const resolvers = {
   Query: {
-    hello: () => "Hello GraphQL !",
-    bye: () => "Bye Graphql !!!!!",
+    products: () => productsData,
   },
 };
 
